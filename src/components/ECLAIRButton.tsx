491fb687e115d04e67c3d21261048cfe07224444
Vishal Kantharaju <vishal.kantharaju@gmail.com>
2023-03-17 16:27:12 -0500

    added gradient shift to eclair button as well

--- src/components/ECLAIRButton.tsx
+++ src/components/ECLAIRButton.tsx
@@ -14,7 +14,7 @@ function ECLAIRButton(props: Props) {
   
 
   return (
-    <button onClick={ handleClick } className =  'fancybutton'>
+    <button onClick={ handleClick } className =  'fancybutton' data-text={ props.text }>
         <span className='fancytext'>
           {props.text}
         </span>
