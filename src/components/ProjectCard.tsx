1bf4d942c978c5fa36ea43595121501d0012dd8f
Vishal Kantharaju <vishal.kantharaju@gmail.com>
2023-03-17 02:44:03 -0500

    card 1.0 fin

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../styles/projectcard.css'
+
+interface Props {
+    img: string;
+    h2: string;
+}
+
+function ProjectCard(props : Props) {
+    return (
+        <div className="entireCard">
+            <img src={props.img} className="cardImage"></img>
+            <div className="cardText">
+                <h2 className="cardTitle">{props.h2}</h2>
+                {/* <p className="cardDesc">{props.p}</p> */}
+            </div>
+            <div className="buttonHolder">
+                <button className="moreButton">
+                    <span className="buttonText">
+                        Learn More
+                    </span>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectCard
