a0bad3ecc6e4ba04a04814b0031cf3ed93c24ec4
Vishal Kantharaju <vishal.kantharaju@gmail.com>
2023-05-07 19:49:37 -0500

    beautified projects page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -20,6 +20,13 @@ import fitnessVid from '../static/videos/fitnessTest.gif';
 import musicVid from '../static/videos/musicTest.gif';
 import robotarmVid from '../static/videos/robotarmTest.gif';
 
+import projectTransHead from '../static/vectors/projectHeaderTransition.svg';
+import projectNormalBG from '../static/vectors/projectNormalBG.svg';
+import projectNormalBG1 from '../static/vectors/projectNormalBG1.svg';
+import projectNormalBG2 from '../static/vectors/projectNormalBG2.svg';
+import projectNormalBG3 from '../static/vectors/projectNormalBG3.png';
+import projectNormalBG4 from '../static/vectors/projectNormalBG4.png';
+
 // interface Info {
 //   id : number;
 //   img : String;
@@ -206,123 +213,137 @@ function Projects() {
 
   return (
     <div className='everything'>
-      <h1 className = 'projectsTitle'>Projects</h1>
-      <div className='filterContainer'>
-        {/* <div className='filterHead'>
-          Filter By Status:
-        </div> */}
-        <ul className='filterOptions'>
-          <li className={`filterOption ${(curFilter.length === 2) ? 'currentSetFilter' : ""}`} onClick={() => handleFilterChange([0, 1])}>All</li>
-          <li className={`filterOption  ${((curFilter.length === 1) && curFilter.includes(0))? 'currentSetFilter' : ""}     `} onClick={() => handleFilterChange([0])}>In-Progress</li>
-          <li className={`filterOption  ${((curFilter.length === 1) && curFilter.includes(1))? 'currentSetFilter' : ""}     `} onClick={() => handleFilterChange([1])}>Completed</li>
-        </ul>
+      <div className='projectPageHeading'>
+        {/* <span className='projectBackToHome'>
+          Home 
+        </span> 
+        <span className='slash'>
+        /
+        </span> */}
+        What We Do 
       </div>
-      <div className='gridContainer'>
-        <div className='projectCards'>
-          {filteredCardData.map((card, index) => (
-            <ImageProjectCard
-              key = {card.id}
-              name = {card.name}
-              img = {card.img}
-              lName  ={card.lName}
-              members = {card.members}
-              desc = {card.desc}
-              git = {card.git}
-              gantt = {card.gantt}
-              vid = {card.video}
-              // ref={cardLocation}
-              ref={el => cardLocations.current[index] = el}
-              onOpenPopup={() => handleOpenPopup(card, cardLocations.current[index]?.getBoundingClientRect())}
-            />
-          ))}
-          {/* <ImageProjectCard 
-            key= {cardData[0].id}
-            name = {cardData[0].name}
-            img = {cardData[0].img}
-            lName  ={cardData[0].lName}
-            members = {cardData[0].members}
-            desc = {cardData[0].desc}
-            git = {cardData[0].git}
-            gantt = {cardData[0].gantt}
-            ref={cardLocation0}
-            onOpenPopup={() => handleOpenPopup(cardData[0], cardLocation0.current?.getBoundingClientRect())}
-            />
-            <ImageProjectCard 
-            key= {cardData[1].id}
-            name = {cardData[1].name}
-            img = {cardData[1].img}
-            lName  ={cardData[1].lName}
-            members = {cardData[1].members}
-            desc = {cardData[1].desc}
-            git = {cardData[1].git}
-            gantt = {cardData[1].gantt}
-            ref={cardLocation1}
-            onOpenPopup={() => handleOpenPopup(cardData[1], cardLocation1.current?.getBoundingClientRect())}
-            />
-            <ImageProjectCard 
-            key= {cardData[2].id}
-            name = {cardData[2].name}
-            img = {cardData[2].img}
-            lName  ={cardData[2].lName}
-            members = {cardData[2].members}
-            desc = {cardData[2].desc}
-            git = {cardData[2].git}
-            gantt = {cardData[2].gantt}
-            ref={cardLocation2}
-            onOpenPopup={() => handleOpenPopup(cardData[2], cardLocation2.current?.getBoundingClientRect())}
-            />
-            <ImageProjectCard 
-            key= {cardData[3].id}
-            name = {cardData[3].name}
-            img = {cardData[3].img}
-            lName  ={cardData[3].lName}
-            members = {cardData[3].members}
-            desc = {cardData[3].desc}
-            git = {cardData[3].git}
-            gantt = {cardData[3].gantt}
-            ref={cardLocation3}
-            onOpenPopup={() => handleOpenPopup(cardData[3], cardLocation3.current?.getBoundingClientRect())}
-            />
-            <ImageProjectCard 
-            key= {cardData[4].id}
-            name = {cardData[4].name}
-            img = {cardData[4].img}
-            lName  ={cardData[4].lName}
-            members = {cardData[4].members}
-            desc = {cardData[4].desc}
-            git = {cardData[4].git}
-            gantt = {cardData[4].gantt}
-            ref={cardLocation4}
-            onOpenPopup={() => handleOpenPopup(cardData[4], cardLocation4.current?.getBoundingClientRect())}
-            />
-            <ImageProjectCard 
-            key= {cardData[5].id}
-            name = {cardData[5].name}
-            img = {cardData[5].img}
-            lName  ={cardData[5].lName}
-            members = {cardData[5].members}
-            desc = {cardData[5].desc}
-            git = {cardData[5].git}
-            gantt = {cardData[5].gantt}
-            ref={cardLocation5}
-            onOpenPopup={() => handleOpenPopup(cardData[5], cardLocation5.current?.getBoundingClientRect())}
-            />
-            <ImageProjectCard 
-            key= {cardData[6].id}
-            name = {cardData[6].name}
-            img = {cardData[6].img}
-            lName  ={cardData[6].lName}
-            members = {cardData[6].members}
-            desc = {cardData[6].desc}
-            git = {cardData[6].git}
-            gantt = {cardData[6].gantt}
-            ref={cardLocation6}
-            onOpenPopup={() => handleOpenPopup(cardData[6], cardLocation6.current?.getBoundingClientRect())}
-            /> */}
+      <div className='projectHeaderTrans' style={{backgroundImage: `url(${projectTransHead})`, height: '10rem', backgroundPosition: 'center', backgroundSize: '200vw'}}>
+
+      </div>
+      <div className='projectPageBelowHeader' style={{backgroundImage: `url(${projectNormalBG4})`, backgroundSize: "200% 100%", height: "100%", backgroundRepeat: "no-repeat", backgroundPosition: 'center'}}>
+        <h1 className = 'projectsTitle'>Projects</h1>
+        <div className='filterContainer'>
+          {/* <div className='filterHead'>
+            Filter By Status:
+          </div> */}
+          <ul className='filterOptions'>
+            <li className={`filterOption ${(curFilter.length === 2) ? 'currentSetFilter' : ""}`} onClick={() => handleFilterChange([0, 1])}>All</li>
+            <li className={`filterOption  ${((curFilter.length === 1) && curFilter.includes(0))? 'currentSetFilter' : ""}     `} onClick={() => handleFilterChange([0])}>In-Progress</li>
+            <li className={`filterOption  ${((curFilter.length === 1) && curFilter.includes(1))? 'currentSetFilter' : ""}     `} onClick={() => handleFilterChange([1])}>Completed</li>
+          </ul>
         </div>
+        <div className='gridContainer'>
+          <div className='projectCards'>
+            {filteredCardData.map((card, index) => (
+              <ImageProjectCard
+                key = {card.id}
+                name = {card.name}
+                img = {card.img}
+                lName  ={card.lName}
+                members = {card.members}
+                desc = {card.desc}
+                git = {card.git}
+                gantt = {card.gantt}
+                vid = {card.video}
+                // ref={cardLocation}
+                ref={el => cardLocations.current[index] = el}
+                onOpenPopup={() => handleOpenPopup(card, cardLocations.current[index]?.getBoundingClientRect())}
+              />
+            ))}
+            {/* <ImageProjectCard 
+              key= {cardData[0].id}
+              name = {cardData[0].name}
+              img = {cardData[0].img}
+              lName  ={cardData[0].lName}
+              members = {cardData[0].members}
+              desc = {cardData[0].desc}
+              git = {cardData[0].git}
+              gantt = {cardData[0].gantt}
+              ref={cardLocation0}
+              onOpenPopup={() => handleOpenPopup(cardData[0], cardLocation0.current?.getBoundingClientRect())}
+              />
+              <ImageProjectCard 
+              key= {cardData[1].id}
+              name = {cardData[1].name}
+              img = {cardData[1].img}
+              lName  ={cardData[1].lName}
+              members = {cardData[1].members}
+              desc = {cardData[1].desc}
+              git = {cardData[1].git}
+              gantt = {cardData[1].gantt}
+              ref={cardLocation1}
+              onOpenPopup={() => handleOpenPopup(cardData[1], cardLocation1.current?.getBoundingClientRect())}
+              />
+              <ImageProjectCard 
+              key= {cardData[2].id}
+              name = {cardData[2].name}
+              img = {cardData[2].img}
+              lName  ={cardData[2].lName}
+              members = {cardData[2].members}
+              desc = {cardData[2].desc}
+              git = {cardData[2].git}
+              gantt = {cardData[2].gantt}
+              ref={cardLocation2}
+              onOpenPopup={() => handleOpenPopup(cardData[2], cardLocation2.current?.getBoundingClientRect())}
+              />
+              <ImageProjectCard 
+              key= {cardData[3].id}
+              name = {cardData[3].name}
+              img = {cardData[3].img}
+              lName  ={cardData[3].lName}
+              members = {cardData[3].members}
+              desc = {cardData[3].desc}
+              git = {cardData[3].git}
+              gantt = {cardData[3].gantt}
+              ref={cardLocation3}
+              onOpenPopup={() => handleOpenPopup(cardData[3], cardLocation3.current?.getBoundingClientRect())}
+              />
+              <ImageProjectCard 
+              key= {cardData[4].id}
+              name = {cardData[4].name}
+              img = {cardData[4].img}
+              lName  ={cardData[4].lName}
+              members = {cardData[4].members}
+              desc = {cardData[4].desc}
+              git = {cardData[4].git}
+              gantt = {cardData[4].gantt}
+              ref={cardLocation4}
+              onOpenPopup={() => handleOpenPopup(cardData[4], cardLocation4.current?.getBoundingClientRect())}
+              />
+              <ImageProjectCard 
+              key= {cardData[5].id}
+              name = {cardData[5].name}
+              img = {cardData[5].img}
+              lName  ={cardData[5].lName}
+              members = {cardData[5].members}
+              desc = {cardData[5].desc}
+              git = {cardData[5].git}
+              gantt = {cardData[5].gantt}
+              ref={cardLocation5}
+              onOpenPopup={() => handleOpenPopup(cardData[5], cardLocation5.current?.getBoundingClientRect())}
+              />
+              <ImageProjectCard 
+              key= {cardData[6].id}
+              name = {cardData[6].name}
+              img = {cardData[6].img}
+              lName  ={cardData[6].lName}
+              members = {cardData[6].members}
+              desc = {cardData[6].desc}
+              git = {cardData[6].git}
+              gantt = {cardData[6].gantt}
+              ref={cardLocation6}
+              onOpenPopup={() => handleOpenPopup(cardData[6], cardLocation6.current?.getBoundingClientRect())}
+              /> */}
+          </div>
       </div>
       <Popup vis ={showPopup} onClose={handleClosePopup} cardInfo={cardInfo} />
       <Box sx = {{paddingBottom:"10%"}}></Box>
+      </div>
     </div>
   )
 }
