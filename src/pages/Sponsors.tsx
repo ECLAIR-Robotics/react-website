143f672d905a112f83e184e541c50ae1ecbefbec
Vishal Kantharaju <vishal.kantharaju@gmail.com>
2023-05-18 15:44:11 +0530

    started sponsors page

--- src/pages/Sponsors.tsx
+++ src/pages/Sponsors.tsx
@@ -1,12 +1,15 @@
 import { Grid } from '@mui/material'
 import React from 'react'
+import spon from '../static/vectors/sponsors9.svg';
 
 function Sponsors() {
   return (
     <div>
-      <Grid>
-        <h1>Members</h1>
-      </Grid>
+      <div className='projectPageHeading'>
+        Sponsors
+      </div>
+      <div className='projectHeaderTrans' style={{backgroundImage: `url(${spon})`, height: '12rem', backgroundPosition: 'center', backgroundSize: '200vw'}}></div>
+      <div style = {{paddingBottom:"7%"}}/>
     </div>
   )
 }
