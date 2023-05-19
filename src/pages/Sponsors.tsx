6f806a39e680143fef0dbf8ca476442150de58d2
Vishal Kantharaju <vishal.kantharaju@gmail.com>
2023-05-19 18:05:11 +0530

    for homie eyes only

--- src/pages/Sponsors.tsx
+++ src/pages/Sponsors.tsx
@@ -1,6 +1,7 @@
 import { Grid } from '@mui/material'
 import React from 'react'
 import spon from '../static/vectors/sponsors9.svg';
+import bg from '../static/vectors/sponsors10.svg'; 
 
 function Sponsors() {
   return (
@@ -9,6 +10,7 @@ function Sponsors() {
         Sponsors
       </div>
       <div className='projectHeaderTrans' style={{backgroundImage: `url(${spon})`, height: '12rem', backgroundPosition: 'center', backgroundSize: '200vw'}}></div>
+      
       <div style = {{paddingBottom:"7%"}}/>
     </div>
   )
