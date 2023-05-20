eeb1b3772ecda3e921f975b5f5443dcbf9677132
Tanay Garg <tanay.garg@utexas.edu>
2023-05-20 20:46:06 +0530

    added a new section to the about us page

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -55,11 +55,7 @@ function Homepage() {
               <ECLAIRButton handleClick={handleClick} text="PROJECTS" />
             </div>
           </div>
-          <div className="custom-shape-divider-bottom-1683336780">
-            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-              <path d="M600,112.77C268.63,112.77,0,65.52,0,7.23V120H1200V7.23C1200,65.52,931.37,112.77,600,112.77Z" className="shape-fill"></path>
-            </svg>
-          </div>
+          
 
         </div>
 
