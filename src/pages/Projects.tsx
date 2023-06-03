6fe5f5e129630741f7987445da2b0c524199cc8b
Vishal Kantharaju <vishal.kantharaju@gmail.com>
2023-06-03 00:10:56 -0500

    added robot arm pic + vid

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -115,7 +115,7 @@ function Projects() {
           image:
             'https://images.unsplash.com/photo-1503177119275-0aa32b3a9368?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
         }
-      ]
+      ] 
     },
     {
       id: 1,
