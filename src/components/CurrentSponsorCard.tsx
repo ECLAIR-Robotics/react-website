0b266e55e8961c1cd9aae500bfd4fd7f7ce7d7d1
ManasAgrawal05 <manas.agrawal2@gmail.com>
2025-01-12 14:53:32 -0600

    Added higher res sponsor images and modified css to handle non-square images

--- src/components/CurrentSponsorCard.tsx
+++ src/components/CurrentSponsorCard.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 
 interface CurrentSponsorCardProps {
   img: string,
+  color: string,
 }
 
 function CurrentSponsorCard(props: CurrentSponsorCardProps) {
 
   return (
-    <img className='currentSponsorCard' src={props.img}/>
+    <img className='currentSponsorCard' src={props.img} style={{backgroundColor: props.color}}/>
   )
 }
 
--- src/components/CurrentSponsorRow.tsx
+++ src/components/CurrentSponsorRow.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 import '../styles/sponsors.css';
 import texelArts from '../static/images/sponsor-images/texelArts.png'
 import texasRobotics from '../static/images/sponsor-images/texasRobotics.jpg'
-import serviceNow from '../static/images/sponsor-images/serviceNow.jpg'
+import serviceNow from '../static/images/sponsor-images/serviceNow.png'
 import CurrentSponsorCard from './CurrentSponsorCard';
 function CurrentSponsorRow() {
 
   return (
     <div className='currentSponsorRow'>
-      <CurrentSponsorCard img={serviceNow}/>
-      <CurrentSponsorCard img={texasRobotics}/>
-      <CurrentSponsorCard img={texelArts}/>
+      <CurrentSponsorCard img={serviceNow} color='rgb(30, 59, 61)'/>
+      <CurrentSponsorCard img={texasRobotics} color='white'/>
+      <CurrentSponsorCard img={texelArts} color='white'/>
     </div>
   )
 }
