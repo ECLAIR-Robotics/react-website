e15b4f48b6c53e6355ed12cc670644a478b46208
Arnav Joshi <74690929+arnav-joshi@users.noreply.github.com>
2026-03-12 16:44:35 -0500

    test commit for decorative items

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -21,7 +21,7 @@ const firebaseConfig = {
   appId:             process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId:     process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
-
+// testing API key security
 
 
 // Initialize Firebase
